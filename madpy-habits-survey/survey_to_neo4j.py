a9bb52d56d7067d7a73b18cea2175446c81228e1
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-05-07 10:29:29 -0500

    Use survey info yaml to download results

--- madpy-habits-survey/survey_to_neo4j.py
+++ madpy-habits-survey/survey_to_neo4j.py
@@ -3,13 +3,10 @@ import google_survey
 
 
 def get_survey_responses():
-    google_sheet_name = 'madpy-habits-responses'
-    creds_file = 'madpy-service-account-key.json'
-    wide_responses = google_survey.get_responses(google_sheet_name, creds_file)
-    responses = google_survey.tidy_responses(wide_responses)
+    responses = google_survey.get('madpy-habits-survey.yaml')
     return responses
 
-    
+
 def connect_to_neo4j():
     neo4j_password = open('neo4j-password.txt').read().strip()
     graph = py2neo.Graph(password=neo4j_password)
