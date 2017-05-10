10d5b99c24e79464cf61fda5dd179599e21697ea
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-05-09 20:55:51 -0500

    Oh man this thing is ugly

--- madpy-habits-survey/you_might_like.py
+++ madpy-habits-survey/you_might_like.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from wtforms import Form, StringField
 app = Flask(__name__)
 
-@app.route('/')
+class MadpyMemberForm(Form):
+    screen_name = StringField('screen_name')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def madpy_habits():
-    return 'Welcome to Madpy!'
+    form = MadpyMemberForm(request.form)
+    if request.method == 'POST' and form.validate():
+        return render_template('recommendations.html')
+    return render_template('home.html', form=form)
